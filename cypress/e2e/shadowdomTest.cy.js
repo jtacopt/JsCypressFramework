11ae19f74e594d45d17e869b60e9805136d7bd24
carvajoa <joao.carvalho@mercedes-benz.io>
2023-02-17 14:46:47 +0000

    ShadowTest

--- cypress/e2e/shadowdomTest.cy.js
+++ cypress/e2e/shadowdomTest.cy.js
@@ -1,20 +1,19 @@
+/// <reference types="cypress" />
 
 describe('Shadow Dom Test', () => {
     beforeEach(() => {
         cy.visit('https://jtacopt.github.io/HTML_Pages/shadow-root.html')
     })
 
-    // https://on.cypress.io/interacting-with-elements
-
-    it('.type() - type into a DOM element', () => {
+    it('Populate login form', () => {
+       
         // https://on.cypress.io/type
         cy.get('login-form').shadow()
             .find('#username')
             .type('username').should('have.value', 'username')
+
         cy.get('login-form').shadow()
             .find('#password')
-            // Ignore error checking prior to type
-            // like whether the input is visible or disabled
             .type('senha')
            
     })
