e1c04350e8f085ce214c689e16bc7a9b24be1db4
carvajoa <joao.carvalho@mercedes-benz.io>
2023-02-17 11:35:00 +0000

    Shadow Dom Test

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,15 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    e2e: {
+
+        viewportHeight: 1080,
+        viewportWight: 1930,
+        specPattern:'cypress/e2e/**/*.{js,ts}',
+        excludeSpecPattern: ['**/1-getting-started/*','**/2-advanced-examples/*'],
+        setupNodeEvents(on, config) {
+          // implement node event listeners here
+          
+        },
   },
 });
